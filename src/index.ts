76ba89bb1ea7277446dbab6f9b46458313beef56
hectorplinio <hectorplinio@gmail.com>
2022-06-06 09:46:08 +0200

    Create server with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import express from "express";
+const app = express();
+const port = Number(process.env.PORT) | 3000;
+app.get("/", (_req, res) => {
+    res.send("Hello World!");
+});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
 console.log("Hello World!");
