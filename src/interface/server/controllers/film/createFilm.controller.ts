9be9464b9dc735cce36ed49c6eeb746d95a599cf
hectorplinio <hectorplinio@gmail.com>
2022-06-07 08:28:40 +0200

    Update createFilm.controller.ts

--- src/interface/server/controllers/film/createFilm.controller.ts
+++ src/interface/server/controllers/film/createFilm.controller.ts
@@ -1,10 +1,12 @@
 import { Film } from "../../../../core/domain/film";
 import FilmORM from "../../../../infraestructure/adapters/films.repository";
 import { Request, Response } from "express";
+import trimFields from "../../../../infraestructure/tools/trim-fields/trim-fields";
+import arrayExceptions from "../../../../infraestructure/tools/trim-fields/array-exceptions";
 
 const createFilmController = async (req: Request, res: Response) => {
     try {
-        const film: Film = req.body;
+        const film: Film = trimFields(req.body, arrayExceptions);
         const newFilm = await FilmORM().createFilm(film);
         console.log(newFilm);
         if (!newFilm.title) {
